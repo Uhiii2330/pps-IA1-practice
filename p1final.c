548ccf22d3b5342dbc1fdf74ae66e69edb10350c
UhithaReva <97276982+Uhiii2330@users.noreply.github.com>
2022-02-01 09:16:43 +0000

    p1 original

---
+++ p1final.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void input(int *a, int *b)
+{
+printf("\n Enter numbers to be added : ");
+scanf("%d %d", &*a, &*b);
+}
+
+void add(int a, int b, int *sum)
+{
+*sum = a + b;
+}
+void output(int a, int b, int sum)
+{
+printf("\n %d + %d = %d", a, b, sum);
+}
+
+
+int main()
+{
+int a, b, sum = 0;
+input(&a, &b);
+add(a, b, &sum);
+output(a, b, sum);
+return 0;
+}
